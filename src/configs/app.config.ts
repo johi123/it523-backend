92e3d6d9c7cada463aa6ea867fa714b60730a06f
Johannes <24382498+johi123@users.noreply.github.com>
2023-01-06 07:50:07 +0100

    fix config issue

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -9,6 +9,6 @@ export const POLYGON_API_TOKEN = process.env.POLYGON_API_TOKEN;
 
 export const TEAM_ID = process.env.TEAM_ID || "66";
 
-if (!FOOTBALL_DATA_API_TOKEN || POLYGON_API_TOKEN) {
+if (!FOOTBALL_DATA_API_TOKEN || !POLYGON_API_TOKEN) {
   throw new Error("Did you forget to add the api keys?");
 }
